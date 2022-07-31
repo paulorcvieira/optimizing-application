877cd14e77f6a14e055a41f23a7a7b7cdbcf10a9
Paulo Vieira <paullocostta86@gmail.com>
2022-07-30 22:36:17 -0300

    Initial

---
+++ src/App.tsx
@@ -0,0 +1,74 @@
+import { useCallback, useState } from 'react';
+
+import { Content } from './components/Content';
+import { SideBar } from './components/SideBar';
+
+import { api } from './services/api';
+
+import './styles/global.scss';
+
+import { useMemo } from 'react';
+import './styles/content.scss';
+import './styles/sidebar.scss';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+export function App() {
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
+
+  useMemo(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  useMemo(() => {
+    api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
+      setMovies(response.data);
+    });
+
+    api.get<GenreResponseProps>(`genres/${selectedGenreId}`).then(response => {
+      setSelectedGenre(response.data);
+    })
+  }, [selectedGenreId]);
+
+
+  const handleClickButton = useCallback((id: number) => {
+    setSelectedGenreId(id);
+  }, [])
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <SideBar
+        genres={genres}
+        selectedGenreId={selectedGenreId}
+        buttonClickCallback={handleClickButton}
+      />
+
+      <Content
+        selectedGenre={selectedGenre}
+        movies={movies}
+      />
+    </div>
+  )
+}
---
+++ src/components/Content.tsx
@@ -0,0 +1,64 @@
+import { memo } from "react";
+import { List, ListRowRenderer } from "react-virtualized";
+import { MovieCard } from "./MovieCard";
+
+interface ContentProps {
+  selectedGenre: {
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
+  };
+
+  movies: Array<{
+    imdbID: string;
+    Title: string;
+    Poster: string;
+    Ratings: Array<{
+      Source: string;
+      Value: string;
+    }>;
+    Runtime: string;
+  }>;
+}
+
+function ContentComponent({ selectedGenre, movies }: ContentProps) {
+
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style} >
+        <MovieCard
+          key={movies[index].imdbID}
+          title={movies[index].Title}
+          poster={movies[index].Poster}
+          runtime={movies[index].Runtime}
+          rating={movies[index].Ratings[0].Value}
+        />
+
+      </div>
+    )
+  }
+
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <List
+
+          height={550}
+          rowHeight={440}
+          width={800}
+          overscanRowCount={1}
+          rowCount={movies.length}
+          rowRenderer={rowRenderer}
+        />
+      </main>
+    </div>
+  )
+}
+
+export const Content = memo(ContentComponent, (prevProps, nextProps) => {
+  return Object.is(prevProps.movies, nextProps.movies);
+})
